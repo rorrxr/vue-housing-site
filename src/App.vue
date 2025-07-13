9200fd467686ac1b1e84e8caa261e17911fb2bca
rorrxr <rorrxr@naver.com>
2025-07-13 17:19:58 +0900

    feat: 반복문을 사용한 menu 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,28 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div>
-      <h4 :style="스타일">{{ products[0] }}</h4>
-      <p>{{ price1 }}만원</p>
-    </div>
-    <div>
-      <h4>{{ products[1] }}</h4>
-      <p>{{ price2 }}만원</p>
-    </div>
-    <div>
-      <h4>{{ products[2] }}</h4>
-      <p>{{ price3 }}만원</p>
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="menu">
+    <a v-for="m in menuItem" :key="m">{{ m }}</a>
+  </div>
+  <div>
+    <h4 :style="스타일">{{ products[0] }}</h4>
+    <p>{{ price1 }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price3 }}만원</p>
+  </div>
 </template>
 
 <script>
-import TheWelcome from './components/TheWelcome.vue'
-
 export default {
   name: 'App',
-  components: {
-    TheWelcome
-  },
   data() {
     return {
-      스타일 : 'color:red',
+      스타일: 'color:red',
+      menuItem: ['Home', 'Shop', 'About'],
       price1: 60,
       price2: 70,
       price3: 80,
@@ -42,30 +33,19 @@ export default {
 </script>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
